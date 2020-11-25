5e276d1377f02ea9451b9d8609fee525ff44eec6
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2020-11-26 01:29:53 +0300

    add x

--- src/main/java/persists/Category.java
+++ src/main/java/persists/Category.java
@@ -27,11 +27,11 @@ public class Category {
     public Category() {
     }
 
-    public Long getiD() {
+    public Long getId() {
         return id;
     }
 
-    public void setiD(Long iD) {
+    public void setId(Long id) {
         this.id = id;
     }
 
